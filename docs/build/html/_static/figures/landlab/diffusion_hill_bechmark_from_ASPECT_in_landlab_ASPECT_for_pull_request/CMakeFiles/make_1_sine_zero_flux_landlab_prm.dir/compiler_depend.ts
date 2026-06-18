53c6162122e2b7841718b5378536ec6e1c9c42a0
biraj <biraj@birajs-Mac-Studio.local>
2026-06-17 21:39:19 -0600

    Stop tracking Sphinx build artifacts

--- docs/build/html/_static/figures/landlab/diffusion_hill_bechmark_from_ASPECT_in_landlab_ASPECT_for_pull_request/CMakeFiles/make_1_sine_zero_flux_landlab_prm.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for make_1_sine_zero_flux_landlab_prm.
--- docs/build/html/_static/figures/landlab/diffusion_hill_bechmark_from_ASPECT_in_landlab_ASPECT_for_pull_request/CMakeFiles/make_1_sine_zero_flux_landlab_prm.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for make_1_sine_zero_flux_landlab_prm.
-# This may be replaced when dependencies are built.
