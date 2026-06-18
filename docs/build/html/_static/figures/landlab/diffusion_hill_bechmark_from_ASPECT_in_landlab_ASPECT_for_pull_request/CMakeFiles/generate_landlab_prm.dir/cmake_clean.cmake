01d298e407d04bce4f115a6e52ad003d2bdb2dba
biraj <biraj@birajs-Mac-Studio.local>
2026-06-18 07:26:06 -0600

    Ignore Sphinx build directory

--- docs/build/html/_static/figures/landlab/diffusion_hill_bechmark_from_ASPECT_in_landlab_ASPECT_for_pull_request/CMakeFiles/generate_landlab_prm.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "1_sine_zero_flux_landlab.prm"
-  "CMakeFiles/generate_landlab_prm"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/generate_landlab_prm.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- docs/build/html/_static/figures/landlab/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# MPI hello world to see if mpi4py works
-
-cd mpi-hello-world
-mpirun -n 2 python test.py
-cd ..
